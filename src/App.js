f4c3bbc1cda32b7220a78241da903767def5c579
Grant Greer <viridiang93@gmail.com>
2022-06-25 06:43:22 -0700

    added button component

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import "./App.css";
+import Button from "./components/Button";
+
+function App() {
+  const [openedEditor, setOpenedEditor] = useState("html");
+  return <div className="App"></div>;
+}
+export default App;
