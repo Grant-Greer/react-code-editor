99679ff34c146f7072fad26807c89b9c4cd979a4
Grant Greer <viridiang93@gmail.com>
2022-06-26 00:31:44 -0700

    added theme dropdown and theme state management

--- src/App.js
+++ src/App.js
@@ -4,6 +4,45 @@ import Button from "./components/Button";
 
 function App() {
   const [openedEditor, setOpenedEditor] = useState("html");
-  return <div className="App"></div>;
+
+  const onTabClick = (editorName) => {
+    setOpenedEditor(editorName);
+  };
+
+  return (
+    <div className="App">
+      <p>Welcome to the editor!</p>
+      <div className="tab-button-container">
+        <Button
+          title="HTML"
+          onClick={() => {
+            onTabClick("html");
+          }}
+        />
+        <Button
+          title="CSS"
+          onClick={() => {
+            onTabClick("css");
+          }}
+        />
+        <Button
+          title="JavaScript"
+          onClick={() => {
+            onTabClick("js");
+          }}
+        />
+      </div>
+
+      <div className="editor-container">
+        {openedEditor === "html" ? (
+          <p>The html editor is open</p>
+        ) : openedEditor === "css" ? (
+          <p>The CSS editor is open!!!!!!</p>
+        ) : (
+          <p>the JavaScript editor is open</p>
+        )}
+      </div>
+    </div>
+  );
 }
 export default App;
